2d56702d9e28c5719e0fe23c839a57817031abb2
(지민) <(hijimin039@gmail.com)>
2024-03-20 20:28:36 +0900

    마이페이지연동안하기로함

--- puppyFriends/src/main/java/com/kh/member/controller/MoveToMypageProductController.java
+++ puppyFriends/src/main/java/com/kh/member/controller/MoveToMypageProductController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MoveToMypageProductController
  */
-@WebServlet("/mypageProduct.cr")
+@WebServlet("/confirmProduct")
 public class MoveToMypageProductController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
